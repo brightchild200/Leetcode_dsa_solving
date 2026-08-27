7940b4953b3043caf9973ed3fbdbddd66053e289
Chaurasiya Riya Sanjay <155414539+brightchild200@users.noreply.github.com>
2026-08-28 02:03:36 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (33.58%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.length();
+        int count =0;
+        if(n==0) return 0;
+        for(int i=n-1; i>=0; i--){
+            if(s[i] ==' ' && count ==0){
+                continue;
+            }else if(s[i] ==' ' && count !=0){
+                break;
+            }else {
+                count++;
+            }  
+        }
+        return count;
+    }
+};
